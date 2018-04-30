4a730babf2ecf1397a9daab3ed71529871f1c1de
Zakaria AIT AADDI <Z.AITAADDI@copag.local>
2018-04-30 12:01:36 +0000

    Change init structure

--- cgc_api/__init__.py
+++ cgc_api/__init__.py
@@ -1,4 +1,23 @@
-from flask import Flask
+from flask import Flask, Blueprint
+from flask_restful import Api
+from cgc_api import resources as r
 
 
 app = Flask(__name__)
+
+# Blueprints
+api_bp = Blueprint('api', __name__)
+
+# APIs
+api = Api(api_bp)
+
+# Add resources
+api.add_resource(r.APIRequest, '/api')
+
+# Register blueprints
+app.register_blueprint(api_bp)
+
+
+# Debugging
+if __name__ == '__main__':
+    app.run(debug=True)
