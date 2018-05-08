8f7063a2c5c76daea164ba9daf7cf51a86ef2250
Zakaria AIT AADDI <Z.AITAADDI@copag.local>
2018-05-08 10:27:38 +0000

    Process "Req_chargement_non_valide"

--- cgc_api/queries.py
+++ cgc_api/queries.py
@@ -1072,7 +1072,7 @@ class Queries(object):
         return query.format(**kwargs)
 
     
-    def Req_chargement_non_valide(self, args):
+    def Req_chargement_non_valide(self, args): #Done
         query = '''
             SELECT 
                 T_CHARGEMENT.DATE_CHARGEMENT AS DATE_CHARGEMENT,	
@@ -1086,15 +1086,27 @@ class Queries(object):
                 T_SECTEUR.code_secteur = T_CHARGEMENT.code_secteur
                 AND
                 (
-                    T_CHARGEMENT.DATE_CHARGEMENT = {Param_date_chargement}
-                    AND	T_CHARGEMENT.VALID = 0
+                    {OPTIONAL_ARG_1}
+                    T_CHARGEMENT.VALID = 0
                 )
             GROUP BY 
                 T_CHARGEMENT.DATE_CHARGEMENT,	
                 T_CHARGEMENT.VALID,	
                 T_SECTEUR.NOM_SECTEUR
         '''
-        return query.format(**kwargs)
+
+        try:
+            kwargs = {
+                'Param_date_chargement': args[0]
+            }
+        except IndexError:
+            raise
+        
+        kwargs['Param_date_chargement'] = self.validateDate(kwargs['Param_date_chargement'])
+        kwargs['OPTIONAL_ARG_1'] = '''T_CHARGEMENT.DATE_CHARGEMENT = '{Param_date_chargement}' AND'''
+        kwargs['OPTIONAL_ARG_1'] = '' if kwargs['Param_date_chargement'] in (None, 'NULL') else kwargs['OPTIONAL_ARG_1']
+
+        return query.format(**kwargs).format(**kwargs)
 
     
     def Req_chargement_par_article(self, args):
