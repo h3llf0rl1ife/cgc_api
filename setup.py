a4e14452d12b187cbd7268e95d7b903b59a4b061
Zakaria AIT AADDI <Z.AITAADDI@copag.local>
2018-05-08 09:53:18 +0000

    Update version

--- setup.py
+++ setup.py
@@ -3,12 +3,13 @@ from distutils.core import setup
 requires = (
     'flask==1.0.1',
     'flask-restful==0.3.6',
-    #'pymssql==2.1.3'
+    'python-dateutil-2.7.2',
+    #'pymssql==2.1.3' install from .wheel instead
 )
 
 setup(
     name='cgc_api',
-    version='0.1',
+    version='0.2',
     packages=['cgc_api'],
     install_requires=requires,
 )
