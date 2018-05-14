bf08b9688de282c699d01ed6d55e1adce04e9e93
Zakaria AIT AADDI <Z.AITAADDI@copag.local>
2018-05-14 09:38:44 +0000

    Process "Req_ls_borderau_valeurs"

--- cgc_api/queries.py
+++ cgc_api/queries.py
@@ -3954,7 +3954,7 @@ class Queries(object):
         return query.format(**kwargs)
 
     
-    def Req_ls_borderau_valeurs(self, args):
+    def Req_ls_borderau_valeurs(self, args): #Done
         query = '''
             SELECT 
                 T_BORDEREAU_VALEURS.ID_BORDEREAU AS ID_BORDEREAU,	
@@ -3976,7 +3976,7 @@ class Queries(object):
             ORDER BY 
                 ID_BORDEREAU DESC
         '''
-        return query.format(**kwargs)
+        return query
 
     
     def Req_ls_chargement_cac(self, args):
