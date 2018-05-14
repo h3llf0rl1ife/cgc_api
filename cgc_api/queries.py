ac3a21ef23116081271f94316caa3539124cd7fd
Zakaria AIT AADDI <Z.AITAADDI@copag.local>
2018-05-14 09:45:27 +0000

    Process "Req_ls_superviseurs"

--- cgc_api/queries.py
+++ cgc_api/queries.py
@@ -6519,7 +6519,7 @@ class Queries(object):
         return query.format(**kwargs)
 
     
-    def Req_ls_superviseurs(self, args):
+    def Req_ls_superviseurs(self, args): #Done
         query = '''
             SELECT 
                 T_OPERATEUR.CODE_OPERATEUR AS CODE_OPERATEUR,	
@@ -6534,7 +6534,7 @@ class Queries(object):
             ORDER BY 
                 NOM_OPERATEUR ASC
         '''
-        return query.format(**kwargs)
+        return query
 
     
     def Req_ls_superviseurs_resp_vente(self, args):
