f8026365589f19a9eb36d3138774940cd2414c51
Zakaria AIT AADDI <Z.AITAADDI@copag.local>
2018-05-15 18:13:41 +0000

    Process "Req_supp_hist_clients"

--- cgc_api/queries.py
+++ cgc_api/queries.py
@@ -10167,13 +10167,26 @@ class Queries(object):
         return query.format(**kwargs)
 
     
-    def Req_supp_hist_clients(self, args):
+    def Req_supp_hist_clients(self, args): #Done
         query = '''
             DELETE FROM 
                 T_MOY_VENTE_CLIENTS
             WHERE 
-                T_MOY_VENTE_CLIENTS.DATE_VENTE = {Param_date_journee}
+                T_MOY_VENTE_CLIENTS.DATE_VENTE = '{Param_date_journee}'
         '''
+        
+        try:
+            kwargs = {
+                'Param_date_journee': args[0]
+            }
+        except IndexError:
+            raise
+
+        kwargs['Param_date_journee'] = self.validateDate(kwargs['Param_date_journee'])
+
+        if kwargs['Param_date_journee'] in (None, 'NULL'):
+            raise ValueError
+
         return query.format(**kwargs)
 
     
