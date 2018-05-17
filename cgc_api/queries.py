718287ca0940901560b191da986113bf9aef54b1
Zakaria AIT AADDI <Z.AITAADDI@copag.local>
2018-05-17 09:08:10 +0000

    Process "Req_supp_statistiques_stock"

--- cgc_api/queries.py
+++ cgc_api/queries.py
@@ -10756,13 +10756,26 @@ class Queries(object):
         return query.format(**kwargs)
 
     
-    def Req_supp_statistiques_stock(self, args):
+    def Req_supp_statistiques_stock(self, args): #Done
         query = '''
             DELETE FROM 
                 T_STATISTIQUES_STOCK
             WHERE 
-                T_STATISTIQUES_STOCK.DATE_JOURNEE = {Param_date_journee}
+                T_STATISTIQUES_STOCK.DATE_JOURNEE = '{Param_date_journee}'
         '''
+
+        try:
+            kwargs = {
+                'Param_date_journee': args[0]
+            }
+        except IndexError:
+            raise
+        
+        kwargs['Param_date_journee'] = self.validateDate(kwargs['Param_date_journee'])
+        
+        if kwargs['Param_date_journee'] in (None, 'NULL'):
+            raise ValueError
+
         return query.format(**kwargs)
 
     
