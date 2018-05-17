2921953fbd12d9882b76bf8bc5ce962600ad4e7d
Zakaria AIT AADDI <Z.AITAADDI@copag.local>
2018-05-17 09:08:37 +0000

    Process "Req_supp_statistiques_clients"

--- cgc_api/queries.py
+++ cgc_api/queries.py
@@ -10745,14 +10745,27 @@ class Queries(object):
         return query.format(**kwargs)
 
     
-    def Req_supp_statistiques_clients(self, args):
+    def Req_supp_statistiques_clients(self, args): #Done
         query = '''
             DELETE FROM 
                 T_STATISTIQUES
             WHERE 
-                T_STATISTIQUES.DATE_JOURNEE = {Param_date_journee}
+                T_STATISTIQUES.DATE_JOURNEE = '{Param_date_journee}'
                 AND	T_STATISTIQUES.CODE_CLIENT <> 0
         '''
+
+        try:
+            kwargs = {
+                'Param_date_journee': args[0]
+            }
+        except IndexError:
+            raise
+        
+        kwargs['Param_date_journee'] = self.validateDate(kwargs['Param_date_journee'])
+        
+        if kwargs['Param_date_journee'] in (None, 'NULL'):
+            raise ValueError
+
         return query.format(**kwargs)
 
     
