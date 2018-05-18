2118f24651d66eb7355726da79a49a89a6b3e2f5
Zakaria AIT AADDI <Z.AITAADDI@copag.local>
2018-05-18 10:43:41 +0000

    Process "Req_valeur_chargements"

--- cgc_api/queries.py
+++ cgc_api/queries.py
@@ -13459,7 +13459,7 @@ class Queries(object):
         return query.format(**kwargs)
 
     
-    def Req_valeur_chargements(self, args):
+    def Req_valeur_chargements(self, args): #Done
         query = '''
             SELECT 
                 T_CHARGEMENT.DATE_CHARGEMENT AS DATE_CHARGEMENT,	
@@ -13480,7 +13480,7 @@ class Queries(object):
                 AND		T_CHARGEMENT.CODE_TOURNEE = T_TOURNEES.CODE_TOURNEE
                 AND
                 (
-                    T_CHARGEMENT.DATE_CHARGEMENT = {Param_date_chargement}
+                    T_CHARGEMENT.DATE_CHARGEMENT = '{Param_date_chargement}'
                     AND	T_CHARGEMENT.VALID = 1
                 )
             GROUP BY 
@@ -13493,6 +13493,19 @@ class Queries(object):
                 T_CHARGEMENT.VALID,	
                 T_OPERATEUR.Matricule
         '''
+        
+        try:
+            kwargs = {
+                'Param_date_chargement': args[0]
+            }
+        except IndexError:
+            raise
+
+        kwargs['Param_date_chargement'] = self.validateDate(kwargs['Param_date_chargement'])
+
+        if kwargs['Param_date_chargement'] in (None, 'NULL'):
+            raise ValueError
+
         return query.format(**kwargs)
 
     
