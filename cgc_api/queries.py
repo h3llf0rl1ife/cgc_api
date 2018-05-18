51b9c3a5ff60e267baed0b23bda0c109d4bfd9c8
Zakaria AIT AADDI <Z.AITAADDI@copag.local>
2018-05-18 10:30:59 +0000

    Process "Req_total_ecart_inventaires"

--- cgc_api/queries.py
+++ cgc_api/queries.py
@@ -11728,7 +11728,7 @@ class Queries(object):
         return query.format(**kwargs)
 
     
-    def Req_total_ecart_inventaires(self, args):
+    def Req_total_ecart_inventaires(self, args): #Done
         query = '''
             SELECT 
                 T_MOUVEMENTS.DATE_MVT AS DATE_MVT,	
@@ -11739,7 +11739,7 @@ class Queries(object):
             FROM 
                 T_MOUVEMENTS
             WHERE 
-                T_MOUVEMENTS.DATE_MVT = {Param_date_mvt}
+                T_MOUVEMENTS.DATE_MVT = '{Param_date_mvt}'
                 AND	T_MOUVEMENTS.TYPE_MOUVEMENT = 'I'
                 AND	T_MOUVEMENTS.TYPE_PRODUIT = 'PRODUIT'
             GROUP BY 
@@ -11748,6 +11748,19 @@ class Queries(object):
                 T_MOUVEMENTS.CODE_ARTICLE,	
                 T_MOUVEMENTS.TYPE_PRODUIT
         '''
+        
+        try:
+            kwargs = {
+                'Param_date_mvt': args[0]
+            }
+        except IndexError:
+            raise
+
+        kwargs['Param_date_mvt'] = self.validateDate(kwargs['Param_date_mvt'])
+
+        if kwargs['Param_date_mvt'] in (None, 'NULL'):
+            raise ValueError
+
         return query.format(**kwargs)
 
     
