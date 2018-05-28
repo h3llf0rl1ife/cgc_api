4fc470ced8b4096f3754c92141a8dc728f79138f
Zakaria AIT AADDI <Z.AITAADDI@copag.local>
2018-05-28 11:20:43 +0000

    Process "Req_releve_client_global_produit"

--- cgc_api/queries.py
+++ cgc_api/queries.py
@@ -11061,7 +11061,7 @@ class Queries(object):
         return query.format(**kwargs)
 
     
-    def Req_releve_client_global_produit(self, args):
+    def Req_releve_client_global_produit(self, args): #Done
         query = '''
             SELECT 
                 T_FACTURE.CODE_CLIENT AS CODE_CLIENT,	
@@ -11089,7 +11089,7 @@ class Queries(object):
                 (
                     T_FACTURE.VALID = 1
                     AND	T_FACTURE.CODE_CLIENT IN ({Param_code_client}) 
-                    AND	T_FACTURE.DATE_HEURE BETWEEN {Param_dt1} AND {Param_dt2}
+                    AND	T_FACTURE.DATE_HEURE BETWEEN '{Param_dt1}' AND '{Param_dt2}'
                 )
             GROUP BY 
                 T_FACTURE.CODE_CLIENT,	
@@ -11100,6 +11100,23 @@ class Queries(object):
                 CODE_CLIENT ASC,	
                 le_minimum_RANG ASC
         '''
+
+        try:
+            kwargs = {
+                'Param_code_client': args[0],
+                'Param_dt1': args[1],
+                'param_3': args[2]
+            }
+        except IndexError as e:
+            return e
+
+        kwargs['Param_dt1'] = self.validateDate(kwargs['Param_dt1'])
+        kwargs['Param_dt2'] = self.validateDate(kwargs['Param_dt2'])
+
+        for key in kwargs:
+            if kwargs[key] in (None, 'NULL'):
+                return ValueError
+
         return query.format(**kwargs)
 
     
