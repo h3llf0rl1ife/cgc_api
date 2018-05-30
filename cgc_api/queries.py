c26abf49e4221687837a4a520700bec3fd6d802f
Zakaria AIT AADDI <Z.AITAADDI@copag.local>
2018-05-30 09:22:03 +0000

    Process "Req_releve_client_tva"

--- cgc_api/queries.py
+++ cgc_api/queries.py
@@ -11207,7 +11207,7 @@ class Queries(object):
         return query.format(**kwargs)
 
     
-    def Req_releve_client_tva(self, args):
+    def Req_releve_client_tva(self, args): #Done
         query = '''
             SELECT 
                 T_FACTURE.CODE_CLIENT AS CODE_CLIENT,	
@@ -11227,8 +11227,8 @@ class Queries(object):
                 AND
                 (
                     T_FACTURE.VALID = 1
-                    AND	T_FACTURE.CODE_CLIENT IN ({Param_code_client}) 
-                    AND	T_FACTURE.DATE_HEURE BETWEEN {Param_dt1} AND {Param_dt2}
+                    {OPTIONAL_ARG_1}
+                    AND	T_FACTURE.DATE_HEURE BETWEEN '{Param_dt1}' AND '{Param_dt2}'
                 )
             GROUP BY 
                 T_FACTURE.CODE_CLIENT,	
@@ -11238,7 +11238,27 @@ class Queries(object):
                 CODE_CLIENT ASC,	
                 TVA ASC
         '''
-        return query.format(**kwargs)
+
+        try:
+            kwargs = {
+                'Param_code_client': args[0],
+                'Param_dt1': args[1],
+                'Param_dt2': args[2]
+            }
+        except IndexError as e:
+            return e
+        
+        kwargs['Param_dt1'] = self.validateDate(kwargs['Param_dt1'])
+        kwargs['Param_dt2'] = self.validateDate(kwargs['Param_dt2'])
+
+        for key in ('Param_dt1', 'Param_dt2'):
+            if kwargs[key] in (None, 'NULL'):
+                return ValueError
+        
+        kwargs['OPTIONAL_ARG_1'] = 'AND	T_FACTURE.CODE_CLIENT IN ({Param_code_client})'
+        kwargs['OPTIONAL_ARG_1'] = '' if kwargs['Param_code_client'] in (None, 'NULL') else kwargs['OPTIONAL_ARG_1']
+
+        return query.format(**kwargs).format(**kwargs)
 
     
     def Req_releve_dons(self, args):
