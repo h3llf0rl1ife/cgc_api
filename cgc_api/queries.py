f69d685b46adf97e2ec71a42c2c3c6ae51934c53
Zakaria AIT AADDI <Z.AITAADDI@copag.local>
2018-06-06 14:40:35 +0000

    Process "Req_update_vendeur_cond"

--- cgc_api/queries.py
+++ cgc_api/queries.py
@@ -15660,7 +15660,7 @@ class Queries(object):
         return query.format(**kwargs)
 
     
-    def Req_update_vendeur_cond(self, args):
+    def Req_update_vendeur_cond(self, args): #Done
         query = '''
             UPDATE 
                 T_COND_CHARGEE
@@ -15669,6 +15669,19 @@ class Queries(object):
             WHERE 
                 T_COND_CHARGEE.CODE_CHARGEMENT = {Param_code_chargement}
         '''
+
+        try:
+            kwargs = {
+                'Param_code_vendeur': args[0],
+                'Param_code_chargement': args[1]
+            }
+        except IndexError as e:
+            return e
+        
+        for key in kwargs:
+            if kwargs[key] in (None, 'NULL'):
+                return ValueError
+    
         return query.format(**kwargs)
 
     
