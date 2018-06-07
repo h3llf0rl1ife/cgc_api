c399f3d3b27853523de31f4963416836ae9825e9
Zakaria AIT AADDI <Z.AITAADDI@copag.local>
2018-06-07 12:18:54 +0000

    Process "Req_moyenne_vente_produit_secteur"

--- cgc_api/queries.py
+++ cgc_api/queries.py
@@ -9578,7 +9578,7 @@ class Queries(object):
         return query.format(**kwargs)
 
     
-    def Req_moyenne_vente_produit_secteur(self, args):
+    def Req_moyenne_vente_produit_secteur(self, args): #Done
         query = '''
             SELECT 
                 T_CHARGEMENT.code_secteur AS code_secteur,	
@@ -9602,17 +9602,39 @@ class Queries(object):
                 AND		T_OPERATEUR.CODE_OPERATEUR[1] = T_CHARGEMENT.code_vendeur
                 AND
                 (
-                    T_CHARGEMENT.DATE_CHARGEMENT BETWEEN {Param_dt1} AND {Param_dt2}
+                    T_CHARGEMENT.DATE_CHARGEMENT BETWEEN '{Param_dt1}' AND '{Param_dt2}'
                     AND	T_CHARGEMENT.code_secteur = {Param_code_secteur}
-                    AND	T_ARTICLES.CODE_PRODUIT = {Param_code_produit}
-                    AND	T_PRODUITS.CODE_FAMILLE = {Param_code_famille}
+                    {OPTIONAL_ARG_1}
+                    {OPTIONAL_ARG_2}
                 )
             GROUP BY 
                 T_CHARGEMENT.code_secteur,	
                 T_PRODUITS.CODE_PRODUIT,	
                 T_OPERATEUR.FONCTION
         '''
-        return query.format(**kwargs)
+
+        try:
+            kwargs = {
+                'Param_dt1': args[0],
+                'Param_dt2': args[1],
+                'Param3': args[2]
+            }
+        except IndexError as e:
+            return e
+        
+        kwargs['Param_dt1'] = self.validateDate(kwargs['Param_dt1'])
+        kwargs['Param_dt2'] = self.validateDate(kwargs['Param_dt2'])
+
+        for key in ('Param_dt1', 'Param_dt2', 'Param_code_secteur'):
+            if kwargs[key] in (None, 'NULL'):
+                return ValueError
+        
+        kwargs['OPTIONAL_ARG_1'] = 'AND	T_ARTICLES.CODE_PRODUIT = {Param_code_produit}'
+        kwargs['OPTIONAL_ARG_1'] = '' if kwargs['Param_code_produit'] in (None, 'NULL') else kwargs['OPTIONAL_ARG_1']
+        kwargs['OPTIONAL_ARG_2'] = 'AND	T_PRODUITS.CODE_FAMILLE = {Param_code_famille}'
+        kwargs['OPTIONAL_ARG_2'] = '' if kwargs['Param_code_famille'] in (None, 'NULL') else kwargs['OPTIONAL_ARG_2']
+        
+        return query.format(**kwargs).format(**kwargs)
 
     
     def Req_mt_a_verser_secteur(self, args): #Done
