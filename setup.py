f8edab563b770b4020f037278bcedcb79769d335
Zakaria AIT AADDI <Z.AITAADDI@copag.local>
2018-06-07 13:12:01 +0000

    Update dependencies and version

--- setup.py
+++ setup.py
@@ -5,11 +5,12 @@ requires = (
     'flask-restful==0.3.6',
     'python-dateutil>=2.7.2',
     #'pymssql==2.1.3' install from .wheel instead
+    'sqlalchemy'
 )
 
 setup(
     name='cgc_api',
-    version='0.2',
+    version='0.3',
     packages=['cgc_api'],
     install_requires=requires,
 )
