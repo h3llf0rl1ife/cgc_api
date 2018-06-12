1d7bfa9ed4402e59942b059f44d0679a292721f6
Zakaria AIT AADDI <Z.AITAADDI@copag.local>
2018-06-12 13:57:11 +0000

    Update dependencies

--- setup.py
+++ setup.py
@@ -2,10 +2,9 @@ from distutils.core import setup
 
 requires = (
     'flask>=1.0.1',
-    'flask-restful==0.3.6',
+    'flask-restful>=0.3.6',
     'python-dateutil>=2.7.2',
     #'pymssql==2.1.3' install from .wheel instead
-    'sqlalchemy'
 )
 
 setup(
