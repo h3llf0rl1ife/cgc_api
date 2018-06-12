9be24f66b30ec3178c8b70a5a1acf65a03179073
Zakaria AIT AADDI <Z.AITAADDI@copag.local>
2018-06-12 13:57:57 +0000

    Remove sqlalchemy and add new api endpoint

--- cgc_api/__init__.py
+++ cgc_api/__init__.py
@@ -1,20 +1,9 @@
 from flask import Flask, Blueprint
 from flask_restful import Api
 
-from sqlalchemy.ext.automap import automap_base
-from sqlalchemy.orm import Session
-from sqlalchemy import create_engine
-import inspect
-
-
 app = Flask(__name__)
 
-# Database mapping
-Base = automap_base()
-engine = create_engine("mssql+pymssql://sqladmin:AcChRgHax2C0p3s@10.7.2.1/TEST_SIEGE?charset=utf8")
-Base.prepare(engine, reflect=True)
-
-# Import resources after mapping the database
+# Import resources
 from cgc_api import resources as r
 
 # Blueprints
@@ -25,7 +14,7 @@ api = Api(api_bp)
 
 # Add resources
 api.add_resource(r.APIRequest, '/api')
-api.add_resource(r.RestfulRequest, '/api/<string:table>')
+api.add_resource(r.ResfulQuery, '/api/<string:table>')
 
 # Register blueprints
 app.register_blueprint(api_bp)
