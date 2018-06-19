d8f98df221c9cff81326e6fb6dee29403b3aac36
Zakaria AIT AADDI <Z.AITAADDI@copag.local>
2018-06-19 09:29:59 +0100

    Fix pep8 problems

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ requires = (
     'flask>=1.0.1',
     'flask-restful>=0.3.6',
     'python-dateutil>=2.7.2',
-    #'pymssql==2.1.3' install from .wheel instead
+    # 'pymssql==2.1.3' install from .wheel instead
 )
 
 setup(
