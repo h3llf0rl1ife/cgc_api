311a8a7893a485e0336210c95e684f8102b0db1c
Zakaria AIT AADDI <Z.AITAADDI@copag.local>
2018-06-19 09:35:55 +0100

    Add url prefix to blueprint

--- cgc_api/__init__.py
+++ cgc_api/__init__.py
@@ -7,14 +7,15 @@ app = Flask(__name__)
 from cgc_api import resources as r
 
 # Blueprints
-api_bp = Blueprint('api', __name__)
+api_bp = Blueprint('api', __name__, url_prefix='/api/v1')
 
 # APIs
 api = Api(api_bp)
 
 # Add resources
-api.add_resource(r.APIRequest, '/api')
-api.add_resource(r.RestfulQuery, '/api/<string:table>')
+api.add_resource(r.APIRequest, '/queries')
+api.add_resource(r.RestfulQuery, '/query/<string:table>')
+api.add_resource(r.RestfulSchema, '/schema', '/')
 
 # Register blueprints
 app.register_blueprint(api_bp)
