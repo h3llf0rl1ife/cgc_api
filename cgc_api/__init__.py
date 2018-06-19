646074c7fd0f2940427aee12b7181bd7ff4b6ae5
Zakaria AIT AADDI <Z.AITAADDI@copag.local>
2018-06-19 10:28:01 +0100

    Fix pep8 problems

--- cgc_api/__init__.py
+++ cgc_api/__init__.py
@@ -1,10 +1,10 @@
 from flask import Flask, Blueprint
 from flask_restful import Api
 
-app = Flask(__name__)
-
-# Import resources
 from cgc_api import resources as r
+
+
+app = Flask(__name__)
 
 # Blueprints
 api_bp = Blueprint('api', __name__, url_prefix='/api/v1')
@@ -22,4 +22,4 @@ app.register_blueprint(api_bp)
 
 # Debugging
 if __name__ == '__main__':
-    app.run(debug=True) #host='0.0.0.0', port=80, 
+    app.run(debug=True)  # host='0.0.0.0', port=80,
--- cgc_api/resources.py
+++ cgc_api/resources.py
@@ -9,10 +9,8 @@ import pymssql
 class APIRequest(Resource):
     queries = Queries(*CURRENT_CONFIG)
 
-
     def get(self):
         pass
-
 
     def post(self):
         keys = {
@@ -22,49 +20,63 @@ class APIRequest(Resource):
         auth = request.authorization
 
         if auth is not None:
-            if (auth.username, auth.password) != (keys['username'], keys['password']):
+            if (auth.username, auth.password) is not (
+                    keys['username'], keys['password']):
                 return {'Status': 401, 'Message': 'Unauthorized access.'}
         else:
             return {'Status': 401, 'Message': 'Unauthorized access.'}
 
         data = request.get_json()
         print('data', data)
-        
+
         try:
             query = getattr(self.queries, data['Parameters']['query'])
         except AttributeError:
-            return {'Status': 404, 'Message': 'Query not found.', 'Client data': data}
+            return {'Status': 404, 'Message': 'Query not found.',
+                    'Client data': data}
         except (KeyError, TypeError):
-            return {'Status': 400, 'Message': 'Bad request.', 'Client data': data}
+            return {'Status': 400, 'Message': 'Bad request.',
+                    'Client data': data}
+
+        # TODO: Add protection againt SQL injection
 
         kwargs = data['Parameters']['kwargs']
         print('query', query(kwargs))
 
         try:
             return self.queries.executeQuery(query(kwargs))
+
         except IndexError:
-            return {'Status': 400, 'Message': 'One or more arguments missing.', 'Client data': data}
+            return {'Status': 400, 'Message': 'One or more arguments missing.',
+                    'Client data': data}
+
         except ValueError:
             raise
+
         except pymssql.ProgrammingError:
-            return {'Status': 400, 'Message': 'Error during query execution.', 'Query': query(kwargs).replace('\t', '').replace('\n', '').replace('            ', '').strip()}
-        
+            query = query(kwargs).replace(
+                '\t', '').replace('\n', '').replace('            ', '').strip()
+
+            return {'Status': 400, 'Message': 'Error during query execution.',
+                    'Query': query}
+
         return {'Status': 400, 'Message': 'Bad request.'}
 
 
 class RestfulQuery(Resource):
     _Query = Query(*CURRENT_CONFIG)
-    
-    
+
     def get(self, table):
         try:
             table = self._Query.validateTable(table)
         except KeyError:
-            return {'Status': 404, 'Message': 'Table not found.', 'Parameters': {'Table': table}}, 404
-    
+            return {'Status': 404, 'Message': 'Table not found.',
+                    'Parameters': {'Table': table}}, 404
+
         except pymssql.OperationalError:
-            return {'Status': 503, 'Message': 'Database connection failed.'}, 503
-        
+            return {'Status': 503,
+                    'Message': 'Database connection failed.'}, 503
+
         data = request.get_json()
         column, value = None, None
 
@@ -72,141 +84,181 @@ class RestfulQuery(Resource):
             try:
                 column = [*data['Parameters']['Select']['Where']][0]
             except KeyError:
-                return {'Status': 400, 'Message': 'Bad request.', 'JSON Data': data}, 400
+                return {'Status': 400, 'Message': 'Bad request.',
+                        'JSON Data': data}, 400
 
             try:
                 column = self._Query.validateColumn(table, column)
             except pymssql.OperationalError:
-                return {'Status': 503, 'Message': 'Database connection failed.'}, 503
+                return {'Status': 503,
+                        'Message': 'Database connection failed.'}, 503
 
             try:
                 value = data['Parameters']['Select']['Where'][column]
             except KeyError:
-                params = {'Table': table, 'Column': [*data['Parameters']['Select']['Where']][0]}
-                return {'Status': 400, 'Message': 'Column not found.', 'Parameters': params}, 400
-            
+                column = [*data['Parameters']['Select']['Where']][0]
+                params = {'Table': table, 'Column': column}
+
+                return {'Status': 400, 'Message': 'Column not found.',
+                        'Parameters': params}, 400
+
         query = self._Query.getRequest(table, column)
 
         try:
             return self._Query.executeQuery(query=query, param=value), 200
-    
+
         except pymssql.OperationalError:
             params = {'Table': table, 'Column': column, 'Value': value}
-            return {'Status': 400, 'Message': 'Error during query execution. Please verify your data.', 'Parameters': params}, 400
+            return {'Status': 400, 'Message': 'Please verify your data.',
+                    'Parameters': params}, 400
 
         except pymssql.ProgrammingError:
-            return {'Status': 500, 'Message': 'Error during query execution.'}, 500
-
+            return {'Status': 500,
+                    'Message': 'Error during query execution.'}, 500
 
     def post(self, table):
         try:
             table = self._Query.validateTable(table)
         except KeyError:
-            return {'Status': 404, 'Message': 'Table not found.', 'Parameters': {'Table': table}}, 404
+            return {'Status': 404, 'Message': 'Table not found.',
+                    'Parameters': {'Table': table}}, 404
 
         except pymssql.OperationalError:
-            return {'Status': 503, 'Message': 'Database connection failed.'}, 503
+            return {'Status': 503,
+                    'Message': 'Database connection failed.'}, 503
 
         data = request.get_json()
 
         try:
             columns = [*data['Parameters']['Insert']['Values']]
         except KeyError:
-            return {'Status': 400, 'Message': 'Bad request.', 'JSON Data': data}, 400
-        
+            return {'Status': 400, 'Message': 'Bad request.',
+                    'JSON Data': data}, 400
+
         except TypeError:
-            return {'Status': 400, 'Message': 'Bad request. Please provide insert values.'}, 400
-        
+            return {'Status': 400,
+                    'Message': 'Please provide insert values.'}, 400
+
         n_columns = len(columns)
 
         try:
             columns = self._Query.validateColumn(table, columns, is_list=True)
         except pymssql.OperationalError:
-            return {'Status': 503, 'Message': 'Database connection failed.'}, 503
+            return {'Status': 503,
+                    'Message': 'Database connection failed.'}, 503
 
         if n_columns != len(columns):
-            params = {'Table': table, 'Columns': [*data['Parameters']['Insert']['Values']]}
-            return {'Status': 400, 'Message': 'No match found for one or more provided columns.', 'Parameters': params}, 400
+            params = {'Table': table,
+                      'Columns': [*data['Parameters']['Insert']['Values']]}
 
-        values = tuple([data['Parameters']['Insert']['Values'][column] for column in columns])
+            return {'Status': 400,
+                    'Message': 'No match found for provided columns.',
+                    'Parameters': params}, 400
+
+        values = data['Parameters']['Insert']['Values']
+        values = tuple([values[column] for column in columns])
         query = self._Query.postRequest(table, columns)
 
         try:
-            row_count = self._Query.executeQuery(query=query, param=values, with_result=False)
-            return {'Status': 200, 'Message': 'Inserted {} record into {}.'.format(row_count, table)}, 200
+            row_count = self._Query.executeQuery(
+                query=query, param=values, with_result=False)
+
+            return {'Status': 200,
+                    'Message': 'Inserted {} record into {}.'.format(
+                        row_count, table)}, 200
 
         except pymssql.OperationalError:
-            params = {'Table': table, 'Values': data['Parameters']['Insert']['Values']}
-            return {'Status': 400, 'Message': 'Error during query execution. Please verify your data.', 'Parameters': params}, 400
+            params = {'Table': table,
+                      'Values': data['Parameters']['Insert']['Values']}
+
+            return {'Status': 400, 'Message': 'Please verify your data.',
+                    'Parameters': params}, 400
 
         except pymssql.ProgrammingError:
-            return {'Status': 500, 'Message': 'Error during query execution.'}, 500
-    
+            return {'Status': 500,
+                    'Message': 'Error during query execution.'}, 500
 
     def put(self, table):
         try:
             table = self._Query.validateTable(table)
         except KeyError:
-            return {'Status': 404, 'Message': 'Table not found.', 'Parameters': {'Table': table}}, 404
+            return {'Status': 404, 'Message': 'Table not found.',
+                    'Parameters': {'Table': table}}, 404
 
         except pymssql.OperationalError:
-            return {'Status': 503, 'Message': 'Database connection failed.'}, 503
+            return {'Status': 503,
+                    'Message': 'Database connection failed.'}, 503
 
         data = request.get_json()
 
         try:
-            update_columns = [*data['Parameters']['Update']['Values']]
-            where_columns = [*data['Parameters']['Update']['Where']]
+            u_columns = [*data['Parameters']['Update']['Values']]
+            w_columns = [*data['Parameters']['Update']['Where']]
         except (KeyError, TypeError):
-            return {'Status': 400, 'Message': 'Bad request.', 'JSON Data': data}, 400
-        
-        n_u_columns, n_w_columns = len(update_columns), len(where_columns)
+            return {'Status': 400, 'Message': 'Bad request.',
+                    'JSON Data': data}, 400
+
+        n_u_columns, n_w_columns = len(u_columns), len(w_columns)
 
         try:
-            update_columns = self._Query.validateColumn(table, update_columns, is_list=True)
-            where_columns = self._Query.validateColumn(table, where_columns, is_list=True)
+            u_columns = self._Query.validateColumn(
+                                  table, u_columns, is_list=True)
+            w_columns = self._Query.validateColumn(
+                                  table, w_columns, is_list=True)
         except pymssql.OperationalError:
-            return {'Status': 503, 'Message': 'Database connection failed.'}, 503
+            return {'Status': 503,
+                    'Message': 'Database connection failed.'}, 503
 
-        for columns, n_columns in zip([update_columns, where_columns], [n_u_columns, n_w_columns]):
+        for columns, n_columns in zip([u_columns, w_columns],
+                                      [n_u_columns, n_w_columns]):
             if n_columns != len(columns):
                 params = {
                     'Table': table, 'Columns': {
-                    'Values': [*data['Parameters']['Update']['Values']],
-                    'Where': [*data['Parameters']['Update']['Where']]
-                }}
-                return {'Status': 400, 'Message': 'No match found for one or more provided columns.', 'Parameters': params}, 400
+                        'Values': [*data['Parameters']['Update']['Values']],
+                        'Where': [*data['Parameters']['Update']['Where']]}
+                        }
+                return {'Status': 400,
+                        'Message': 'No match found for provided columns.',
+                        'Parameters': params}, 400
 
-        update_values = tuple([data['Parameters']['Update']['Values'][column] for column in update_columns])
-        where_values = tuple([data['Parameters']['Update']['Where'][column] for column in where_columns])
-        values = update_values + where_values
-        
-        query = self._Query.putRequest(table, update_columns, where_columns)
+        u_values = data['Parameters']['Update']['Values']
+        u_values = tuple([u_values[column] for column in u_columns])
+        w_values = data['Parameters']['Update']['Where']
+        w_values = tuple([w_values[column] for column in w_columns])
+        values = u_values + w_values
+
+        query = self._Query.putRequest(table, u_columns, w_columns)
 
         try:
-            row_count = self._Query.executeQuery(query=query, param=values, with_result=False)
-            return {'Status': 200, 'Message': 'Updated {} record in {}.'.format(row_count, table)}, 200
+            row_count = self._Query.executeQuery(
+                query=query, param=values, with_result=False)
+            return {'Status': 200,
+                    'Message': 'Updated {} record in {}.'.format(
+                        row_count, table)}, 200
 
         except pymssql.OperationalError:
             params = {
                     'Table': table, 'Columns': {
-                    'Values': data['Parameters']['Update']['Values'],
-                    'Where': data['Parameters']['Update']['Where']
-                }}
-            return {'Status': 400, 'Message': 'Error during query execution. Please verify your data.', 'Parameters': params}, 400
+                        'Values': data['Parameters']['Update']['Values'],
+                        'Where': data['Parameters']['Update']['Where']}
+                        }
+            return {'Status': 400, 'Message': 'Please verify your data.',
+                    'Parameters': params}, 400
 
         except pymssql.ProgrammingError:
-            return {'Status': 500, 'Message': 'Error during query execution.'}, 500
+            return {'Status': 500,
+                    'Message': 'Error during query execution.'}, 500
 
-    
     def delete(self, table):
         try:
             table = self._Query.validateTable(table)
         except KeyError:
-            return {'Status': 404, 'Message': 'Table not found.', 'Parameters': {'Table': table}}, 404
+            return {'Status': 404, 'Message': 'Table not found.',
+                    'Parameters': {'Table': table}}, 404
 
         except pymssql.OperationalError:
-            return {'Status': 503, 'Message': 'Database connection failed.'}, 503
+            return {'Status': 503,
+                    'Message': 'Database connection failed.'}, 503
 
         data = request.get_json()
         columns, values = None, None
@@ -215,32 +267,45 @@ class RestfulQuery(Resource):
             try:
                 columns = [*data['Parameters']['Delete']['Where']]
             except KeyError:
-                return {'Status': 400, 'Message': 'Bad request.', 'JSON Data': data}, 400
+                return {'Status': 400, 'Message': 'Bad request.',
+                        'JSON Data': data}, 400
 
             try:
-                columns = self._Query.validateColumn(table, columns, is_list=True)
+                columns = self._Query.validateColumn(
+                                table, columns, is_list=True)
             except pymssql.OperationalError:
-                return {'Status': 503, 'Message': 'Database connection failed.'}, 503
+                return {'Status': 503,
+                        'Message': 'Database connection failed.'}, 503
 
             try:
-                values = tuple([data['Parameters']['Delete']['Where'][column] for column in columns])
+                values = data['Parameters']['Delete']['Where']
+                values = tuple([values[column] for column in columns])
             except KeyError:
-                params = {'Table': table, 'Columns': [*data['Parameters']['Delete']['Where']]}
-                return {'Status': 400, 'Message': 'No match found for one or more provided columns.', 'Parameters': params}, 400
-            
+                params = {'Table': table,
+                          'Columns': [*data['Parameters']['Delete']['Where']]}
+
+                return {'Status': 400,
+                        'Message': 'No match found for provided columns.',
+                        'Parameters': params}, 400
+
         query = self._Query.deleteRequest(table, columns)
 
         try:
-            row_count = self._Query.executeQuery(query=query, param=values, with_result=False)
-            return {'Status': 200, 'Message': 'Deleted {} records from {}.'.format(row_count, table)}, 200
+            row_count = self._Query.executeQuery(
+                query=query, param=values, with_result=False)
+
+            return {'Status': 200,
+                    'Message': 'Deleted {} records from {}.'.format(
+                        row_count, table)}, 200
 
         except pymssql.OperationalError:
             params = {'Table': table, 'Columns': columns, 'Values': values}
-            return {'Status': 400, 'Message': 'Error during query execution. Please verify your data.', 'Parameters': params}, 400
+            return {'Status': 400, 'Message': 'Please verify your data.',
+                    'Parameters': params}, 400
 
         except pymssql.ProgrammingError:
-            return {'Status': 500, 'Message': 'Error during query execution.'}, 500
-            return {'Status': 500, 'Message': 'Error during query execution.'}, 500
+            return {'Status': 500,
+                    'Message': 'Error during query execution.'}, 500
 
 
 class RestfulSchema(Resource):
