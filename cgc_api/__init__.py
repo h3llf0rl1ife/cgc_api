8000ca5ddd732cf1015a81cba71d24030fce367a
Zakaria AIT AADDI <Z.AITAADDI@copag.local>
2018-06-20 14:16:10 +0100

    Update resource urls and add v0 resources

--- cgc_api/__init__.py
+++ cgc_api/__init__.py
@@ -8,15 +8,19 @@ from cgc_api.config import DEBUG_CONFIG
 app = Flask(__name__)
 
 # Blueprints
-api_bp = Blueprint('api', __name__, url_prefix='/api/v1')
+api_bp = Blueprint('api', __name__, url_prefix='/api')
 
 # APIs
-api = Api(api_bp)
+api_v1 = Api(api_bp, '/v1')
+api_v0 = Api(api_bp, '/v0')
 
 # Add resources
-api.add_resource(r.APIRequest, '/queries')
-api.add_resource(r.RestfulQuery, '/query/<string:table>')
-api.add_resource(r.RestfulSchema, '/schema', '/')
+api_v1.add_resource(r.APIRequest, '/queries')
+api_v1.add_resource(r.RestfulQuery, '/query/<string:table>')
+api_v1.add_resource(r.RestfulSchemaV1, '/schema', '/')
+
+api_v0.add_resource(r.TemporaryRedirect, '/query/<string:table>')
+api_v0.add_resource(r.RestfulSchemaV0, '/schema', '/')
 
 # Register blueprints
 app.register_blueprint(api_bp)
