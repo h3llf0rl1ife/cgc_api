196deb7be679ac45cf06201700ceb070712216cc
Zakaria AIT AADDI <Z.AITAADDI@copag.local>
2018-06-29 19:41:11 +0100

    Configure JWT endpoint

--- cgc_api/auth.py
+++ cgc_api/auth.py
@@ -1,29 +1,40 @@
+import os
+import datetime
+
 from flask import request
 from flask_restful import Resource
 
 from cgc_api.config import CURRENT_CONFIG, SECRET
 from cgc_api.query import Query
 from cgc_api.crypto import Crypto
+from cgc_api import models as m
+from cgc_api import db
+
+sqlserver = Query(*CURRENT_CONFIG)
 
 
-class AccessAuth(Resource):
+def getDate():
+    return datetime.date.today().isoformat()
+
+
+class Auth(Resource):
     def get(self):
-        pass
+        return {'Date': getDate()}
 
     def post(self):
         auth = request.data
 
         if auth:
-            crypto = Crypto(SECRET)
+            crypto = Crypto(SECRET + getDate())
             hash_str = crypto.b64decode(auth)
             expected_hash = crypto.hashString(
-                'sha256', crypto.secret, crypto.secret_key)
+                'sha512', crypto.secret, crypto.secret_key)
 
             hash_challenge = crypto.checkHashString(hash_str, expected_hash)
 
             if hash_challenge:
                 query = 'SELECT CODE_OPERATEUR, FONCTION FROM T_OPERATEUR'
-                operators = Query(*CURRENT_CONFIG).executeQuery(query)
+                operators = sqlserver.executeQuery(query)
                 return operators
 
         return {}
@@ -34,8 +45,31 @@ class Token(Resource):
         jwt = request.data.decode('ansi')
 
         if jwt:
-            crypto = Crypto(SECRET)
+            crypto = Crypto(SECRET + getDate())
             payload = crypto.readJWT(jwt)
-            return payload
+
+            if payload:
+                machine = m.Machine.query.filter_by(
+                    MachineName=payload['Machine']).first()
+                operator = m.Operator.query.filter_by(
+                    OperatorCode=payload['Operator']).first()
+
+                if machine and operator:
+                    token = m.Token.query.filter_by(
+                        Machine=machine, Operator=operator,
+                        IssuedAt=getDate(), ExpiresAt=getDate()).first()
+
+                    if token:
+                        return {'Token': token.TokenHash}
+
+                    token_hash = crypto.generateToken(64)
+                    token = m.Token(
+                        TokenHash=token_hash, Active=True,
+                        Machine=machine, Operator=operator,
+                        IssuedAt=getDate(), ExpiresAt=getDate())
+                    db.session.add(token)
+                    db.session.commit()
+
+                    return {'Token': token_hash}
 
         return {}
