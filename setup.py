98e35a2bfc31295a308bc43fa9c6df11b2cc21cc
Zakaria AIT AADDI <Z.AITAADDI@copag.local>
2018-07-16 10:10:10 +0100

    Update dependencies and version

--- setup.py
+++ setup.py
@@ -1,15 +1,17 @@
 from distutils.core import setup
 
 requires = (
-    'flask>=1.0.1',
+    'flask>=1.0',
     'flask-restful>=0.3.6',
     'python-dateutil>=2.7.2',
+    'cryptography',
+    'flask-sqlalchemy',
     # 'pymssql==2.1.3' install from .wheel instead
 )
 
 setup(
     name='cgc_api',
-    version='0.3',
+    version='1.0',
     packages=['cgc_api'],
     install_requires=requires,
 )
